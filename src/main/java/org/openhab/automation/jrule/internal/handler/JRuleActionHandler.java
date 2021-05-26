94c37582e74ef334ac0d14f389556bfb30cc01a7
Joseph Hagberg <joseph@zoidberg.se>
2021-05-26 08:02:26 +0200

    Fixed classpath for ext-lib when executing rules, fixed issue with reload and monitored items
    Former-commit-id: c3850d2f5e876973df34323d64bafb0d43aae47a

---
+++ src/main/java/org/openhab/automation/jrule/internal/handler/JRuleActionHandler.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.automation.jrule.internal.handler;
+
+import java.time.Duration;
+
+import org.openhab.core.io.net.exec.ExecUtil;
+
+/**
+ * The {@link JRuleEventHandler} is responsible for handling commands and status
+ * updates for JRule
+ *
+ * @author Joseph (Seaside) Hagberg - Initial contribution
+ */
+public class JRuleActionHandler {
+
+    private static JRuleActionHandler instance = null;
+
+    private JRuleActionHandler() {
+    }
+
+    public static JRuleActionHandler get() {
+        if (instance == null) {
+            instance = new JRuleActionHandler();
+        }
+        return instance;
+    }
+
+    public void executeCommandLine(String... commandLine) {
+        ExecUtil.executeCommandLine(commandLine);
+    }
+
+    public String executeCommandAndAwaitResponse(long delayInSeconds, String... commandLine) {
+        return ExecUtil.executeCommandLineAndWaitResponse(Duration.ofSeconds(delayInSeconds), commandLine);
+    }
+}
