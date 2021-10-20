11e707b8164dd3ada456774781ee3cf4b0212c6a
Joseph Hagberg <joseph@zoidberg.se>
2021-10-20 07:59:02 +0200

    Added coloritem
    Former-commit-id: 5234d6808ef70606e2d00f7aa4457068ad9303ea

---
+++ src/main/java/org/openhab/automation/jrule/items/JRuleColorItem.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.automation.jrule.items;
+
+import org.openhab.automation.jrule.internal.handler.JRuleEventHandler;
+import org.openhab.automation.jrule.rules.value.JRuleColorValue;
+
+/**
+ * The {@link JRuleColorItem} Items
+ *
+ * @author Joseph (Seaside) Hagberg - Initial contribution
+ */
+public abstract class JRuleColorItem extends JRuleItem {
+
+    public static JRuleColorValue getState(String itemName) {
+        return JRuleEventHandler.get().getColorValue(itemName);
+    }
+
+    public static void sendCommand(String itemName, JRuleColorValue colorValue) {
+        JRuleEventHandler.get().sendCommand(itemName, colorValue);
+    }
+
+    public static void postUpdate(String itemName, JRuleColorValue colorValue) {
+        JRuleEventHandler.get().postUpdate(itemName, colorValue);
+    }
+}
---
+++ src/main/java/org/openhab/automation/jrule/rules/value/JRuleHsbValue.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.automation.jrule.rules.value;
+
+/**
+ * The {@link JRuleColorValue} JRule Command
+ *
+ * @author Joseph (Seaside) Hagberg - Initial contribution
+ */
+public class JRuleHsbValue {
+
+    @Override
+    public String toString() {
+        return "JRuleHsbValue [hue=" + hue + ", saturation=" + saturation + ", brightness=" + brightness + "]";
+    }
+
+    private final int hue;
+    private final int saturation;
+    private final int brightness;
+
+    public JRuleHsbValue(int hue, int saturation, int brightness) {
+        this.hue = hue;
+        this.saturation = saturation;
+        this.brightness = brightness;
+    }
+
+    public int getHue() {
+        return hue;
+    }
+
+    public int getBrightness() {
+        return brightness;
+    }
+
+    public int getSaturation() {
+        return saturation;
+    }
+}
