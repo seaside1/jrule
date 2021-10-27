b4104ebb353d99f89fe985a4c14e6128c5c8ffac
Timo Litzius <dev@dbzman-online.eu>
2021-10-27 22:44:34 +0200

    Add support for OnOff and Percent commands + sate in ColorItem
    Former-commit-id: 30c049c6291ce6100a119dfe52e8befe24f5c8d9

--- src/main/java/org/openhab/automation/jrule/items/JRuleColorItem.java
+++ src/main/java/org/openhab/automation/jrule/items/JRuleColorItem.java
@@ -14,6 +14,7 @@ package org.openhab.automation.jrule.items;
 
 import org.openhab.automation.jrule.internal.handler.JRuleEventHandler;
 import org.openhab.automation.jrule.rules.value.JRuleColorValue;
+import org.openhab.automation.jrule.rules.value.JRuleOnOffValue;
 
 /**
  * The {@link JRuleColorItem} Items
@@ -26,11 +27,35 @@ public abstract class JRuleColorItem extends JRuleItem {
         return JRuleEventHandler.get().getColorValue(itemName);
     }
 
+    public static JRuleOnOffValue getOnOffState(String itemName) {
+        return JRuleEventHandler.get().getOnOffValue(itemName);
+    }
+
+    public static int getPercentState(String itemName) {
+        return JRuleEventHandler.get().getStateFromItemAsInt(itemName);
+    }
+
     public static void sendCommand(String itemName, JRuleColorValue colorValue) {
         JRuleEventHandler.get().sendCommand(itemName, colorValue);
     }
 
+    public static void sendCommand(String itemName, JRuleOnOffValue command) {
+        JRuleEventHandler.get().sendCommand(itemName, command);
+    }
+
+    public static void sendCommand(String itemName, int value) {
+        JRuleEventHandler.get().sendCommand(itemName, new JRulePercentType(value));
+    }
+
     public static void postUpdate(String itemName, JRuleColorValue colorValue) {
         JRuleEventHandler.get().postUpdate(itemName, colorValue);
+    }
+
+    public static void postUpdate(String itemName, JRuleOnOffValue state) {
+        JRuleEventHandler.get().postUpdate(itemName, state);
+    }
+
+    public static void postUpdate(String itemName, int value) {
+        JRuleEventHandler.get().postUpdate(itemName, new JRulePercentType(value));
     }
 }
