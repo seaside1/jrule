8feea1e35952f886174f687bd0d79474de8a772d
Arne Seime <arne.seime@gmail.com>
2022-10-10 19:41:05 +0200

    Add mandatory license and javadoc

--- src/test/java/org/openhab/binding/jrule/internal/QuantityTypeParsingTest.java
+++ src/test/java/org/openhab/binding/jrule/internal/QuantityTypeParsingTest.java
@@ -1,3 +1,15 @@
+/**
+ * Copyright (c) 2010-2022 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.openhab.binding.jrule.internal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,6 +19,11 @@ import org.junit.jupiter.api.Test;
 import org.openhab.core.library.types.QuantityType;
 import org.openhab.core.library.unit.SIUnits;
 
+/**
+ * The {@link QuantityTypeParsingTest}
+ *
+ * @author Arne Seime - Initial contribution
+ */
 public class QuantityTypeParsingTest {
 
     @Test
