06a0e79999b4e10f5c68434a80a4e7a19747273c
querdenker2k <querdenker2k@gmx.de>
2022-11-05 15:10:23 +0100

    added integration test

---
+++ src/test/java/org/openhab/automation/jrule/rules/integration_test/ITJRule.java
@@ -0,0 +1,151 @@
+/**
+ * Copyright (c) 2010-2022 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.automation.jrule.rules.integration_test;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.awaitility.Awaitility;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.openhab.automation.jrule.items.JRuleSwitchItem;
+import org.openhab.automation.jrule.rules.user.TestRules;
+
+/**
+ * The {@link ITJRule}
+ *
+ * @author Robert Delbrück - Initial contribution
+ */
+// Please do not reuse items for testing, create new ones, otherwise every change will hurt sometimes
+public class ITJRule extends JRuleITBase {
+    @Test
+    public void switchItemReceiveCommand() throws IOException {
+        sendCommand(TestRules.ITEM_RECEIVING_COMMAND_SWITCH, JRuleSwitchItem.ON);
+        verifyRuleWasExecuted(TestRules.NAME_SWITCH_ITEM_RECEIVED_ANY_COMMAND);
+        verifyRuleWasExecuted(TestRules.NAME_SWITCH_ITEM_RECEIVED_ANY_UPDATE);
+        verifyRuleWasExecuted(TestRules.NAME_SWITCH_ITEM_CHANGED);
+    }
+
+    @Test
+    public void switchItemReceiveOnCommand() throws IOException {
+        sendCommand(TestRules.ITEM_RECEIVING_COMMAND_SWITCH, JRuleSwitchItem.ON);
+        verifyRuleWasExecuted(TestRules.NAME_SWITCH_ITEM_RECEIVED_ON_COMMAND);
+        verifyRuleWasExecuted(TestRules.NAME_SWITCH_ITEM_RECEIVED_ON_UPDATE);
+        verifyRuleWasExecuted(TestRules.NAME_SWITCH_ITEM_CHANGED_TO_ON);
+    }
+
+    @Test
+    public void invokeMqttAction() throws IOException {
+        sendCommand(TestRules.ITEM_MQTT_ACTION_TRIGGER, JRuleSwitchItem.ON);
+        verifyRuleWasExecuted(TestRules.NAME_INVOKE_MQTT_ACTION);
+        verifyMqttMessageReceived("1313131");
+    }
+
+    @Test
+    public void executeCommandLine() throws IOException, InterruptedException {
+        sendCommand(TestRules.ITEM_COMMANDLINE_EXEC_TRIGGER, JRuleSwitchItem.ON);
+        verifyRuleWasExecuted(TestRules.NAME_EXEC_COMMAND_LINE);
+        verifyFileExist();
+    }
+
+    @Test
+    public void mqttChannelTriggered() throws MqttException {
+        publishMqttMessage("number/state", "123");
+        verifyRuleWasExecuted(TestRules.NAME_MQTT_CHANNEL_TRIGGERED);
+    }
+
+    @Test
+    public void preconditionExecution() throws IOException {
+        sendCommand(TestRules.ITEM_PRECONDITION_STRING, "will not match");
+        sendCommand(TestRules.ITEM_PRECONDITIONED_SWITCH, JRuleSwitchItem.ON);
+        verifyRuleWasNotExecuted(TestRules.NAME_PRECONDITION_EXECUTION);
+
+        sendCommand(TestRules.ITEM_PRECONDITION_STRING, "that matches");
+        sendCommand(TestRules.ITEM_PRECONDITIONED_SWITCH, JRuleSwitchItem.ON);
+        verifyRuleWasExecuted(TestRules.NAME_PRECONDITION_EXECUTION);
+    }
+
+    @Test
+    public void mqttThingChangedToOffline() {
+        Awaitility.await().with().pollDelay(1, TimeUnit.SECONDS).timeout(10, TimeUnit.SECONDS)
+                .pollInterval(200, TimeUnit.MILLISECONDS).await("thing online")
+                .until(() -> getThingState("mqtt:topic:mqtt:generic"), s -> s.equals("ONLINE"));
+        mqttProxy.setConnectionCut(true);
+        Awaitility.await().with().pollDelay(1, TimeUnit.SECONDS).timeout(20, TimeUnit.SECONDS)
+                .pollInterval(200, TimeUnit.MILLISECONDS).await("thing offline")
+                .until(() -> getThingState("mqtt:topic:mqtt:generic"), s -> s.equals("OFFLINE"));
+        verifyRuleWasExecuted(TestRules.NAME_MQTT_THING_CHANGED_TO_OFFLINE);
+    }
+
+    @Disabled("not merged currently")
+    @Test
+    public void memberOfGroupReceivedCommand() throws IOException {
+        sendCommand(TestRules.ITEM_SWITCH_GROUP_MEMBER1, JRuleSwitchItem.ON);
+        verifyRuleWasExecuted(TestRules.NAME_MEMBER_OF_GROUP_RECEIVED_COMMAND);
+        clearLog();
+        sendCommand(TestRules.ITEM_SWITCH_GROUP_MEMBER1, JRuleSwitchItem.ON);
+        verifyRuleWasExecuted(TestRules.NAME_MEMBER_OF_GROUP_RECEIVED_COMMAND);
+        clearLog();
+        sendCommand(TestRules.ITEM_SWITCH_GROUP_MEMBER2, JRuleSwitchItem.ON);
+        verifyRuleWasExecuted(TestRules.NAME_MEMBER_OF_GROUP_RECEIVED_COMMAND);
+        clearLog();
+        sendCommand(TestRules.ITEM_SWITCH_GROUP_MEMBER2, JRuleSwitchItem.ON);
+        verifyRuleWasExecuted(TestRules.NAME_MEMBER_OF_GROUP_RECEIVED_COMMAND);
+    }
+
+    @Disabled("not merged currently")
+    @Test
+    public void memberOfGroupReceivedUpdate() throws IOException {
+        postUpdate(TestRules.ITEM_SWITCH_GROUP_MEMBER1, JRuleSwitchItem.ON);
+        verifyRuleWasExecuted(TestRules.NAME_MEMBER_OF_GROUP_RECEIVED_UPDATE);
+        clearLog();
+        postUpdate(TestRules.ITEM_SWITCH_GROUP_MEMBER1, JRuleSwitchItem.ON);
+        verifyRuleWasExecuted(TestRules.NAME_MEMBER_OF_GROUP_RECEIVED_UPDATE);
+        clearLog();
+        postUpdate(TestRules.ITEM_SWITCH_GROUP_MEMBER2, JRuleSwitchItem.ON);
+        verifyRuleWasExecuted(TestRules.NAME_MEMBER_OF_GROUP_RECEIVED_UPDATE);
+        clearLog();
+        postUpdate(TestRules.ITEM_SWITCH_GROUP_MEMBER2, JRuleSwitchItem.ON);
+        verifyRuleWasExecuted(TestRules.NAME_MEMBER_OF_GROUP_RECEIVED_UPDATE);
+    }
+
+    @Disabled("not merged currently")
+    @Test
+    public void memberOfGroupChanged() throws IOException {
+        postUpdate(TestRules.ITEM_SWITCH_GROUP_MEMBER1, JRuleSwitchItem.ON);
+        verifyRuleWasExecuted(TestRules.NAME_MEMBER_OF_GROUP_CHANGED);
+        clearLog();
+        postUpdate(TestRules.ITEM_SWITCH_GROUP_MEMBER2, JRuleSwitchItem.ON);
+        verifyRuleWasExecuted(TestRules.NAME_MEMBER_OF_GROUP_CHANGED);
+    }
+
+    @Test
+    public void conditionLteAndGteForNumber() throws IOException {
+        sendCommand(TestRules.ITEM_NUMBER_CONDITION, "21");
+        verifyRuleWasNotExecuted(TestRules.NAME_PRECONDITION_LTE_AND_GTE_FOR_NUMBER);
+        clearLog();
+        sendCommand(TestRules.ITEM_NUMBER_CONDITION, "17");
+        verifyRuleWasNotExecuted(TestRules.NAME_PRECONDITION_LTE_AND_GTE_FOR_NUMBER);
+        clearLog();
+        sendCommand(TestRules.ITEM_NUMBER_CONDITION, "20");
+        verifyRuleWasExecuted(TestRules.NAME_PRECONDITION_LTE_AND_GTE_FOR_NUMBER);
+        sendCommand(TestRules.ITEM_NUMBER_CONDITION, "18");
+        verifyRuleWasExecuted(TestRules.NAME_PRECONDITION_LTE_AND_GTE_FOR_NUMBER);
+    }
+
+    @Test
+    public void cronEvery5Sec() {
+        verifyRuleWasExecuted(TestRules.NAME_CRON_EVERY_5_SEC);
+    }
+}
