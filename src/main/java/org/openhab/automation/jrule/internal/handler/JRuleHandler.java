de40566580cfa0c325df2e4ae6708166433723ab
Joseph Hagberg <joseph@zoidberg.se>
2022-11-07 21:22:57 +0100

    Reverted back

--- src/main/java/org/openhab/automation/jrule/internal/handler/JRuleHandler.java
+++ src/main/java/org/openhab/automation/jrule/internal/handler/JRuleHandler.java
@@ -509,12 +509,15 @@ public class JRuleHandler implements PropertyChangeListener {
             try {
                 for (URL url : getURLs()) {
                     if (url.getProtocol().equals("file")) {
-                        FileInputStream is = new FileInputStream(
-                                url.getFile() + "/" + name.replaceAll("\\.", "/") + JRuleConstants.CLASS_FILE_TYPE);
-                        if (is != null) {
-                            byte[] buf = is.readAllBytes();
-                            is.close();
-                            return defineClass(name, is.readAllBytes(), 0, buf.length);
+                        File classFile = new File(url.getPath(),
+                                name.replaceAll("\\.", "/") + JRuleConstants.CLASS_FILE_TYPE);
+                        if (classFile.exists()) {
+                            FileInputStream is = new FileInputStream(classFile);
+                            if (is != null) {
+                                byte[] buf = is.readAllBytes();
+                                is.close();
+                                return defineClass(name, buf, 0, buf.length);
+                            }
                         }
                     }
                 }
