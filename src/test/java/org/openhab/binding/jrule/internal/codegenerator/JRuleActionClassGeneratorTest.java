0c0345f5adeb008002ecc81870bc987c45227568
querdenker2k <querdenker2k@gmx.de>
2022-11-10 22:21:32 +0100

    fix inheritance in actions

--- src/test/java/org/openhab/binding/jrule/internal/codegenerator/JRuleActionClassGeneratorTest.java
+++ src/test/java/org/openhab/binding/jrule/internal/codegenerator/JRuleActionClassGeneratorTest.java
@@ -15,6 +15,12 @@ package org.openhab.binding.jrule.internal.codegenerator;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
@@ -23,20 +29,27 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.openhab.automation.jrule.actions.JRuleActionClassGenerator;
 import org.openhab.automation.jrule.internal.JRuleConfig;
 import org.openhab.automation.jrule.internal.compiler.JRuleCompiler;
 import org.openhab.binding.jrule.internal.thingaction.MyThingActions;
+import org.openhab.core.library.types.StringType;
 import org.openhab.core.magic.binding.handler.MagicActionModuleThingHandler;
+import org.openhab.core.model.script.internal.engine.action.ThingActionService;
 import org.openhab.core.thing.Thing;
+import org.openhab.core.thing.ThingRegistry;
 import org.openhab.core.thing.ThingTypeUID;
 import org.openhab.core.thing.ThingUID;
 import org.openhab.core.thing.binding.ThingHandlerService;
 import org.openhab.core.thing.internal.ThingImpl;
+import org.openhab.core.types.Command;
 
 /**
  * The {@link JRuleActionClassGeneratorTest}
@@ -84,7 +97,8 @@ public class JRuleActionClassGeneratorTest {
     }
 
     @Test
-    public void testGenerateActionsFile() {
+    public void testGenerateActionsFile() throws ClassNotFoundException, MalformedURLException, NoSuchFieldException,
+            NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         Thing thing1 = new ThingImpl(new ThingTypeUID("mybinding", "thingtype"),
                 new ThingUID("mybinding", "thingtype", "id1"));
         thing1.setHandler(new MagicActionModuleThingHandler(thing1) {
@@ -114,6 +128,24 @@ public class JRuleActionClassGeneratorTest {
 
         File compiledClass = new File(targetFolder, "JRuleActions.class");
         assertTrue(compiledClass.exists());
+
+        MockedStatic<ThingActionService> thingActionServiceMock = Mockito.mockStatic(ThingActionService.class);
+        Mockito.when(ThingActionService.getActions(Mockito.any(), Mockito.any())).thenReturn(new MyThingActions());
+        Thing thingMock = Mockito.mock(Thing.class);
+        Mockito.when(thingMock.getHandler()).thenReturn(new MagicActionModuleThingHandler(thingMock));
+        Mockito.when(Mockito.mock(ThingRegistry.class).get(Mockito.any())).thenReturn(thingMock);
+
+        URLClassLoader classLoader = new URLClassLoader(new URL[] { new File("target/gen").toURI().toURL() },
+                JRuleActionClassGeneratorTest.class.getClassLoader());
+        final String className = "org.openhab.automation.jrule.generated.actions.JRuleActions";
+        // compiler.loadClass(classLoader, className, true);
+        Class<?> aClass = classLoader.loadClass(className);
+        Object jRuleActions = aClass.getConstructor().newInstance();
+        Field mybindingThingtypeId1 = aClass.getDeclaredField("mybindingThingtypeId1");
+        Object action = mybindingThingtypeId1.get(jRuleActions);
+        Method doSomethingAbstract = action.getClass().getDeclaredMethod("doSomethingAbstract", Command.class);
+        Object res = doSomethingAbstract.invoke(action, new StringType("blub"));
+        Assertions.assertEquals(10, res);
     }
 
     private void generateAndCompile(Thing thing) {
