1b9ca1dbf77e759dc9881b64eea33199b79909bf
querdenker2k <querdenker2k@gmx.de>
2022-11-20 21:03:56 +0100

    stable testing

--- src/test/java/org/openhab/automation/jrule/rules/integration_test/ITJRule.java
+++ src/test/java/org/openhab/automation/jrule/rules/integration_test/ITJRule.java
@@ -1,13 +1,13 @@
 /**
  * Copyright (c) 2010-2022 Contributors to the openHAB project
- *
+ * <p>
  * See the NOTICE file(s) distributed with this work for additional
  * information.
- *
+ * <p>
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0
- *
+ * <p>
  * SPDX-License-Identifier: EPL-2.0
  */
 package org.openhab.automation.jrule.rules.integration_test;
@@ -18,6 +18,7 @@ import java.util.concurrent.TimeUnit;
 import org.awaitility.Awaitility;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.junit.jupiter.api.Test;
+import org.junitpioneer.jupiter.RetryingTest;
 import org.openhab.automation.jrule.items.JRuleSwitchItem;
 import org.openhab.automation.jrule.rules.user.TestRules;
 
@@ -27,8 +28,11 @@ import org.openhab.automation.jrule.rules.user.TestRules;
  * @author Robert Delbrück - Initial contribution
  */
 // Please do not reuse items for testing, create new ones, otherwise every change will hurt sometimes
+// RetryingTest is used because sometimes the container log output splits a logline into 2 lines and therefor the
+// verifyRuleExectuted will not work.
 public class ITJRule extends JRuleITBase {
     @Test
+    @RetryingTest(2)
     public void switchItemReceiveCommand() throws IOException {
         sendCommand(TestRules.ITEM_RECEIVING_COMMAND_SWITCH, JRuleSwitchItem.ON);
         verifyRuleWasExecuted(TestRules.NAME_SWITCH_ITEM_RECEIVED_ANY_COMMAND);
@@ -37,6 +41,7 @@ public class ITJRule extends JRuleITBase {
     }
 
     @Test
+    @RetryingTest(2)
     public void switchItemReceiveOnCommand() throws IOException {
         sendCommand(TestRules.ITEM_RECEIVING_COMMAND_SWITCH, JRuleSwitchItem.ON);
         verifyRuleWasExecuted(TestRules.NAME_SWITCH_ITEM_RECEIVED_ON_COMMAND);
@@ -45,6 +50,7 @@ public class ITJRule extends JRuleITBase {
     }
 
     @Test
+    @RetryingTest(2)
     public void invokeMqttAction() throws IOException {
         sendCommand(TestRules.ITEM_MQTT_ACTION_TRIGGER, JRuleSwitchItem.ON);
         verifyRuleWasExecuted(TestRules.NAME_INVOKE_MQTT_ACTION);
@@ -52,6 +58,7 @@ public class ITJRule extends JRuleITBase {
     }
 
     @Test
+    @RetryingTest(2)
     public void executeCommandLine() throws IOException, InterruptedException {
         sendCommand(TestRules.ITEM_COMMANDLINE_EXEC_TRIGGER, JRuleSwitchItem.ON);
         verifyRuleWasExecuted(TestRules.NAME_EXEC_COMMAND_LINE);
@@ -59,12 +66,14 @@ public class ITJRule extends JRuleITBase {
     }
 
     @Test
+    @RetryingTest(2)
     public void mqttChannelTriggered() throws MqttException {
         publishMqttMessage("number/state", "123");
         verifyRuleWasExecuted(TestRules.NAME_MQTT_CHANNEL_TRIGGERED);
     }
 
     @Test
+    @RetryingTest(2)
     public void preconditionExecution() throws IOException, InterruptedException {
         sendCommand(TestRules.ITEM_PRECONDITION_STRING, "will not match");
         sendCommand(TestRules.ITEM_PRECONDITIONED_SWITCH, JRuleSwitchItem.ON);
@@ -77,6 +86,7 @@ public class ITJRule extends JRuleITBase {
     }
 
     @Test
+    @RetryingTest(2)
     public void mqttThingChangedToOffline() {
         Awaitility.await().with().pollDelay(1, TimeUnit.SECONDS).timeout(20, TimeUnit.SECONDS)
                 .pollInterval(200, TimeUnit.MILLISECONDS).await("thing online")
@@ -89,6 +99,7 @@ public class ITJRule extends JRuleITBase {
     }
 
     @Test
+    @RetryingTest(2)
     public void memberOfGroupReceivedCommand() throws IOException {
         sendCommand(TestRules.ITEM_SWITCH_GROUP_MEMBER1, JRuleSwitchItem.ON);
         verifyRuleWasExecuted(TestRules.NAME_MEMBER_OF_GROUP_RECEIVED_COMMAND);
@@ -104,6 +115,7 @@ public class ITJRule extends JRuleITBase {
     }
 
     @Test
+    @RetryingTest(2)
     public void memberOfGroupReceivedUpdate() throws IOException {
         postUpdate(TestRules.ITEM_SWITCH_GROUP_MEMBER1, JRuleSwitchItem.ON);
         verifyRuleWasExecuted(TestRules.NAME_MEMBER_OF_GROUP_RECEIVED_UPDATE);
@@ -119,6 +131,7 @@ public class ITJRule extends JRuleITBase {
     }
 
     @Test
+    @RetryingTest(2)
     public void memberOfGroupChanged() throws IOException {
         postUpdate(TestRules.ITEM_SWITCH_GROUP_MEMBER1, JRuleSwitchItem.ON);
         verifyRuleWasExecuted(TestRules.NAME_MEMBER_OF_GROUP_CHANGED);
@@ -128,6 +141,7 @@ public class ITJRule extends JRuleITBase {
     }
 
     @Test
+    @RetryingTest(2)
     public void conditionLteAndGteForNumber() throws IOException {
         sendCommand(TestRules.ITEM_NUMBER_CONDITION, "21");
         verifyRuleWasNotExecuted(TestRules.NAME_PRECONDITION_LTE_AND_GTE_FOR_NUMBER);
@@ -142,6 +156,7 @@ public class ITJRule extends JRuleITBase {
     }
 
     @Test
+    @RetryingTest(2)
     public void membersOfGroup() throws IOException {
         sendCommand(TestRules.ITEM_GET_MEMBERS_OF_GROUP_SWITCH, JRuleSwitchItem.ON);
         verifyRuleWasExecuted(TestRules.NAME_GET_MEMBERS_OF_GROUP);
@@ -149,6 +164,7 @@ public class ITJRule extends JRuleITBase {
     }
 
     @Test
+    @RetryingTest(2)
     public void membersOfNumberGroup() throws IOException {
         sendCommand(TestRules.ITEM_GET_MEMBERS_OF_GROUP_SWITCH, JRuleSwitchItem.ON);
         verifyRuleWasExecuted(TestRules.NAME_GET_MEMBERS_OF_NUMBER_GROUP);
@@ -156,11 +172,13 @@ public class ITJRule extends JRuleITBase {
     }
 
     @Test
+    @RetryingTest(2)
     public void cronEvery5Sec() {
         verifyRuleWasExecuted(TestRules.NAME_CRON_EVERY_5_SEC);
     }
 
     @Test
+    @RetryingTest(2)
     public void castAllTypes() throws IOException {
         sendCommand(TestRules.ITEM_CAST_ALL_TYPES_SWITCH, JRuleSwitchItem.ON);
         verifyRuleWasExecuted(TestRules.NAME_CAST_ALL_TYPES);
@@ -199,6 +217,7 @@ public class ITJRule extends JRuleITBase {
     }
 
     @Test
+    @RetryingTest(2)
     public void triggerRuleFromRule() throws IOException {
         sendCommand(TestRules.ITEM_RULE_FROM_RULE, JRuleSwitchItem.ON);
         verifyRuleWasExecuted(TestRules.NAME_TRIGGER_RULE_FROM_RULE);
@@ -207,6 +226,7 @@ public class ITJRule extends JRuleITBase {
     }
 
     @Test
+    @RetryingTest(2)
     public void nullTesting() throws IOException {
         sendCommand(TestRules.ITEM_TRIGGER_RULE, TestRules.COMMAND_NULL_TESTING);
         verifyRuleWasExecuted(TestRules.NAME_NULL_TESTING);
