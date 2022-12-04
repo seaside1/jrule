22f68170ba93c6975aaf3e6967ba50c5a2b7aafa
querdenker2k <querdenker2k@gmx.de>
2022-12-04 11:28:12 +0100

    remove netty dep

--- src/main/java/org/openhab/automation/jrule/internal/handler/JRuleTimerHandler.java
+++ src/main/java/org/openhab/automation/jrule/internal/handler/JRuleTimerHandler.java
@@ -1,13 +1,13 @@
 /**
  * Copyright (c) 2010-2022 Contributors to the openHAB project
- *
+ * <p>
  * See the NOTICE file(s) distributed with this work for additional
  * information.
- *
+ * <p>
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0
- *
+ * <p>
  * SPDX-License-Identifier: EPL-2.0
  */
 package org.openhab.automation.jrule.internal.handler;
@@ -37,8 +37,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 
-import io.netty.util.concurrent.DefaultThreadFactory;
-
 /**
  * The {@link JRuleTimerHandler} handles all things with internal rule timers
  *
@@ -63,7 +61,7 @@ public class JRuleTimerHandler {
     private final CopyOnWriteArrayList<JRuleTimer> timers = new CopyOnWriteArrayList<>();
 
     private static final ExecutorService executorService = Executors
-            .newCachedThreadPool(new DefaultThreadFactory("jrule-timer", true));
+            .newCachedThreadPool(target -> new Thread(target, "jrule-timer"));
 
     private JRuleTimerHandler() {
     }
