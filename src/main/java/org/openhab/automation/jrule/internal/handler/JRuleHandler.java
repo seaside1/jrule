0a80804c97a623124512a6f019cffcbdf1e1f7f9
Geir Eilertsen <eilertsen@corporater.com>
2025-09-26 13:46:53 +0200

    Ensure proper closing of classloader in JRuleHandler to release resources.

--- src/main/java/org/openhab/automation/jrule/internal/handler/JRuleHandler.java
+++ src/main/java/org/openhab/automation/jrule/internal/handler/JRuleHandler.java
@@ -312,6 +312,13 @@ public class JRuleHandler implements PropertyChangeListener {
         compiler.loadClassesFromFolder(loader, new File(config.getRulesRootDirectory()), config.getRulesPackage(),
                 true);
         compiler.loadClassesFromJar(loader, new File(config.getJarRulesDirectory()), config.getRulesPackage(), true);
+
+        try {
+            //Close classloader. This will propagate to all classes that implement Closeable
+            loader.close();
+        } catch (IOException e) {
+            logger.error("Could not close classLoader", e);
+        }
     }
 
     private boolean initializeFolder(String folder) {
