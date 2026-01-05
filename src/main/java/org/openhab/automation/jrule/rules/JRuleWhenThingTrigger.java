ab5df1a9401d7d3579c29ffe0c7d75a3fb7e1247
Arne Seime <arne.seime@gmail.com>
2026-01-05 20:55:12 +0100

    Revert annotation changes

--- src/main/java/org/openhab/automation/jrule/rules/JRuleWhenThingTrigger.java
+++ src/main/java/org/openhab/automation/jrule/rules/JRuleWhenThingTrigger.java
@@ -12,7 +12,11 @@
  */
 package org.openhab.automation.jrule.rules;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 import org.openhab.automation.jrule.things.JRuleThingStatus;
 
